f3862258db962a0195a88e7ee5294ae85881e012
Eliron Lubaton <elironlub12@gmail.com>
2025-07-13 18:26:13 +0300

    Minor changes

--- main.py
+++ main.py
@@ -24,67 +24,9 @@ topologies_dict = {} # Holds all the different topologies masks
 dir_path = 'C:/Users/eliron.lubaton/Desktop/SolidCAM/CodePy/JSONs'
 
 drilling_types = ["NC_DRILL_OLD", "NC_DRILL_DEEP", "NC_THREAD", "NC_DRILL_HR", "NC_JOB_MW_DRILL_5X"]
-non_drilling_types = ["NC_PROFILE", "NC_CHAMFER"]
+non_drilling_types = ["NC_PROFILE", "NC_CHAMFER", "NC_JOB_HSS_PARALLEL_TO_CURVE"]
 
 ### Processing loop ###
-
-# def processing_loop():
-#     """
-#     This function goes over pair of files: JSON file and a corresponding EXCEL file (which contains
-#     more information on the JSON file).
-#     """
-#     # Going over all subdirectories inside dir_path
-#     for subdir_name in os.listdir(dir_path):
-#         subdir_path = os.path.join(dir_path, subdir_name)
-#
-#         # Skip if not a directory
-#         if not os.path.isdir(subdir_path):
-#             continue
-#
-#         # Initialize file paths
-#         json_file_path = None
-#         excel_file_path = None
-#
-#         # Search for .json and .xls files inside the subdirectory
-#         for file_name in os.listdir(subdir_path):
-#             if file_name.endswith('.json'):
-#                 json_file_path = os.path.join(subdir_path, file_name)
-#             elif file_name.endswith('.xls') or file_name.endswith('.xlsx'):
-#                 excel_file_path = os.path.join(subdir_path, file_name)
-#
-#         if json_file_path is None or excel_file_path is None:
-#             print(f"Missing .json or .xls file in {subdir_name}")
-#             continue
-#
-#         # Read and process the JSON file
-#         data = read_json(json_file_path)
-#
-#         # Checking if the data is in inch or mm
-#         is_inch = 1 if (data["event_data"]["part"]["is_inch"] == True) else 0
-#
-#         # Processing the JSON file - Going over all jobs in the part
-#         print(f"Processing part: {subdir_name}")
-#         for job in data["event_data"]["jobs"]:
-#             # # If the data is in inch, converting it to mm
-#             # if is_inch:
-#             #   job = convert_json_units(job)
-#
-#             # Making sure all the relevant fields in the JSON exist and are correct
-#             validate_job(job, json_file_path)
-#
-#             # Checking if the job is not pre-drilling for creating pockets
-#             if job["geometry"].get("recognized_holes_groups") is not None:
-#                 # Processing the job
-#                 process_jobs(job, json_file_path, topologies_dict)
-#
-#
-#         # Processing the EXCEL file
-#         process_excel_files(excel_file_path)
-#
-#         print(f"\n***********************\n")
-
-def process_excel_files(file_path):
-    return
 
 
 def processing_loop():
@@ -105,7 +47,7 @@ def processing_loop():
                     continue
 
                 # Making sure all the relevant fields in the JSON exist and are correct
-                validate_job(job, part_name)
+                # validate_job(job, part_name)
 
                 # Checking if the job is not pre-drilling for creating pockets
                 if job["geometry"].get("recognized_holes_groups") is not None:
@@ -113,25 +55,10 @@ def processing_loop():
                     process_jobs(job, part_name, topologies_dict)
             print(f"\n***********************\n")
 
-
 processing_loop()
 
 
 
-
-
-
-          # # True if the job is one of the drilling jobs
-          # if job["type"] in drilling_types:
-          #   validate_job(job) # sending True because it's a drilling job
-          #   # Checking if the job is not pre-drilling for creating pockets
-          #   if "recognized_holes_groups" in job['geometry']:
-          #     process_drilling_jobs(job, part_name, topologies_dict)
-          #
-          # else:
-          #   # The job is NOT a drilling job - for now, refers to Profile and Chamfer
-          #   validate_job(job) # sending False because it's NOT a drilling job
-          #   process_non_drilling_jobs(job, topologies_dict)
 
 
 
